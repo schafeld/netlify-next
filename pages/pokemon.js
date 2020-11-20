2e75bccec7157129c3475c892b7785ccbbe989c9
Oliver Schafeld <oliver@schafeld.com>
2020-11-20 16:23:41 +0100

    add Pokemon view with fetch from API

---
+++ pages/pokemon.js
@@ -0,0 +1,20 @@
+function Pokemon({ pokemon }) {
+  return (
+    <div>
+      Look, it's {pokemon?.name}!
+      <img src={pokemon?.sprites.front_default} />
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/charmander')
+  const pokemon = await res.json()
+
+  return {
+    props: {
+      pokemon
+    }
+  }
+}
+export default Pokemon
