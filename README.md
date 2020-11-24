c02cd0cc2414a7f253e40a9af4c86616883ae389
Oliver Schafeld <oliver@schafeld.com>
2020-11-24 19:12:00 +0100

    add dynamic routes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # netlify-next
 Simple demo/boilerplate for a Next.js project run on Netlify, based on: https://explorers.netlify.com/learn/nextjs
+
+## Usage
+
+Test dynamic paths like this: http://localhost:3000/pokemon/bulbasaur
---
+++ pages/pokemon/[pokemon].js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+
+function Pokemon({ pokemon }) {
+  return (
+    <>
+    <Head>
+      <title>Pokemon: {pokemon?.name}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+      <div>
+        Well, that's a {pokemon?.name}!
+        <img src={pokemon?.sprites.front_default} />
+      </div>
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  const pokemon = await res.json()
+
+  let paths = pokemon.results.map(p => {
+    return `/pokemon/${p.name}`
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.pokemon}`)
+  const pokemon = await res.json()
+
+  return {
+    props: {
+      pokemon
+    }
+  }
+}
+export default Pokemon
